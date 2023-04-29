cf2a79e5abc8f22b14bdd23f6f68992c40afabf8
gabriel streck <gabriel@apperto.co>
2023-04-29 20:40:03 -0300

    add icons in manifest

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,8 +17,27 @@ const App = ({ Component, pageProps }: AppProps) => {
         <Head>
           <title>PokeDev</title>
           <meta name='description' content='Created by Gabriel Streck' />
-          <meta name='viewport' content='width=device-width, initial-scale=1' />
           <link rel='icon' href='/favicon.ico' />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="theme-color" content="transparent" />
+          <link rel="manifest" href="/manifest.json" />
+          <link
+            rel="apple-touch-icon"
+            sizes="76x76"
+            href="/icons/icon-76x76.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/icons/icon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/icons/icon-16x16.png"
+          />
         </Head>
         <Component {...pageProps} toggleDarkMode={toggleDarkMode} />
       </ThemeProvider>
