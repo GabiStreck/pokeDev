d51e1845312900b0f858468ec63ff375a6f7a025
gabriel streck <gabriel@apperto.co>
2023-05-03 14:57:36 -0300

    add test for useGenres and remove useDebounceFilte

---
+++ tests/hooks/useGenres.test.ts
@@ -0,0 +1,49 @@
+import { renderHook, waitFor } from "@testing-library/react";
+import useGenres from "@/hooks/useGenres";
+import { GenresResponse } from "@/types/genres";
+import fetchMock from "jest-fetch-mock";
+
+describe("Hooks: useGenres", () => {
+    beforeEach(() => {
+        fetchMock.resetMocks();
+    });
+
+    it("Should return the genre with the correct parameters", async () => {
+        const mockGenresData: GenresResponse = {
+            id: 1,
+            name: "male",
+            pokemon_species_details: [],
+            required_for_evolution: [],
+        };
+        fetchMock.mockResponseOnce(JSON.stringify(mockGenresData));
+        const { result } = renderHook(() => useGenres("male"));
+        expect(result.current.loading).toBe(true);
+
+        await waitFor(() =>
+            expect(result.current.loading).toBe(false)
+        )
+        expect(result.current.genres).not.toBeNull();
+        expect(result.current.genres?.name).toEqual(mockGenresData.name);
+    });
+
+    it("Does not fetch genres data when no genre is provided", async () => {
+        const { result } = renderHook(() => useGenres(""));
+        expect(result.current.loading).toBe(false);
+        expect(result.current.genres).toBeNull();
+        expect(fetchMock).not.toHaveBeenCalled();
+    });
+
+    it("Should return null when wrong data is provided", async () => {
+        const errorMessage = "Error fetching genres data";
+        fetchMock.mockReject(new Error(errorMessage));
+        const { result } = renderHook(() =>
+            useGenres("test-error")
+        );
+        expect(result.current.loading).toBe(true);
+        await waitFor(() =>
+            expect(result.current.loading).toBe(false)
+        )
+
+        expect(result.current.genres).toBeNull();
+    });
+});
